55c2ca06a1556eac453d7f999b13c596f22e539d
Shreshtha Awasthi <shreshtha.awasthi229@gmail.com>
2026-02-07 23:15:59 +0530

    // yo yo

---
+++ Day07.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci number is: %d\n", fib(n));
+
+    return 0;
+}
